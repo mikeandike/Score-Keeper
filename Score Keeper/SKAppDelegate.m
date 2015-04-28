e6a86289bf0fca1c62320d9b0ba26e5df73f47b1
Jon Thornburg <reliable2192@gmail.com>
2015-04-28 14:01:50 -0600

    The Start

--- Score Keeper/SKAppDelegate.m
+++ Score Keeper/SKAppDelegate.m
@@ -7,13 +7,14 @@
 //
 
 #import "SKAppDelegate.h"
+#import "SKViewController.h"
 
 @implementation SKAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+    self.window.rootViewController = [SKViewController new];
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ Score Keeper/SKViewController.h
@@ -0,0 +1,13 @@
+//
+//  SKViewController.h
+//  Score Keeper
+//
+//  Created by jonathan thornburg on 4/28/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKViewController : UIViewController
+
+@end
